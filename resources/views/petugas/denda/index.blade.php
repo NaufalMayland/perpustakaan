f8f97fa15b03699a53cd725415d9228c097f0453
NaufalMayland <maylandzaa13@gmail.com>
2025-02-07 13:37:47 +0700

    style dataTable

--- resources/views/petugas/denda/index.blade.php
+++ resources/views/petugas/denda/index.blade.php
@@ -2,83 +2,75 @@
 @section('content')
     <div class="bg-white p-4 rounded shadow-md">
         <div class="flex justify-between items-center">
-            <div class="font-bold text-lg w-full">
-                <span>Data {{ $title }}</span>
-            </div>
-            <div class="flex gap-2 text-white text-sm w-full">
-                <a href="" target="_blank" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-print"></i>
-                    <span>Print</span>
-                </a>
-                <a href="" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-file-export"></i>
-                    <span>Eksport</span>
-                </a>
-                <a href="" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-file-import"></i>
-                    <span>Import</span>
-                </a>
-                <a href="" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-file-import"></i>
-                    <span>Tambah</span>
+            <h2 class="font-semibold text-lg">Data {{ $title }}</h2>
+            <div class="flex gap-2 text-sm">
+                <a href="" target="_blank" class="px-4 py-2 rounded bg-blue-900 text-white flex items-center gap-2 hover:bg-blue-950">
+                    <i class="fa-solid fa-print"></i> Print
                 </a>
             </div>
         </div>
-        <div class="mt-4">
-            <table class="text-sm" id="dendaTable">
-                <thead class="w-full">
-                    <tr>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Buku Pinjaman</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Peminjam</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">status</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">option</td>
+    </div>
+    <div class="bg-white p-4 rounded shadow-md mt-4">
+        <div class="overflow-x-auto">
+            <table id="dendaTable" class="min-w-full border border-gray-400 text-sm">
+                <thead class="bg-gray-400 w-full">
+                    <tr class="text-gray-950">
+                        <th class="p-2 text-center font-bold uppercase relative">Peminjam</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Buku Pinjaman</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Status</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Option</th>
                     </tr>
                 </thead>
                 <tbody class="w-full">
-                    <tr>
-                        <td>..</td>
-                        <td>..</td>
-                        <td>..</td>
-                        <td class="p-2 flex gap-2 text-center justify-center">
-                            <a href="" class="py-1 px-2 rounded text-center bg-blue-500 text-white">
-                                <i class="fa-solid fa-pencil text-sm"></i>
-                            </a>
-                            <form id="deleteDenda" action="" method="POST" class="hidden">
-                                @csrf
-                                @method('DELETE')
-                            </form>
-                            <button class="py-1 px-2 rounded text-center bg-red-500 text-white" onclick="deleteDenda()">
-                                <i class="fa-solid fa-trash text-sm"></i>    
-                            </button>
-                        </td>
-                    </tr>
+                    {{-- @foreach ($dataBuku as $item) --}}
+                        <tr class="border-b border-gray-400 hover:bg-gray-50">
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2">
+                                <div class="flex gap-2 justify-center items-center">
+                                    <a href="" class="py-1 px-2 rounded text-center bg-blue-900 hover:bg-blue-950 text-white">
+                                        <i class="fa-solid fa-pencil text-sm"></i>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    {{-- @endforeach --}}
                 </tbody>
             </table>
         </div>
     </div>
-    bagian print belum
+
+    <style>
+        .dataTables_wrapper .dataTables_filter, 
+        .dataTables_wrapper .dataTables_length,
+        .dataTables_wrapper .dataTables_info,
+        .dataTables_wrapper .dataTables_paginate {
+            font-size: 0.25rem !important; 
+        }
+
+        th {
+            text-align: left !important;    
+        }
+    </style>
 
     <script>
-        $(document).ready( function () {
-            $('#dendaTable').DataTable();
-            
-        } );
-
-        function deleteDenda() {
-            Swal.fire({
-            title: 'Hapus',
-            text: "Anda yakin untuk logout?",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#2563eb',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes'
-            })
-            .then((result) => {
-                if (result.isConfirmed) {
-                document.getElementById('deleteDenda').submit();
+        $(document).ready(function() {
+            $('#dendaTable').DataTable({
+                responsive: true,
+                autoWidth: false,
+                language: {
+                    search: "Cari:",
+                    lengthMenu: "Show _MENU_",
+                    info: "Show _START_ to _END_ of _TOTAL_ entries",
+                    paginate: {
+                        first: "<<",
+                        last: ">>",
+                        next: ">",
+                        previous: "<"
+                    }
                 }
             });
-        }
+        });
     </script>
 @endsection
--- resources/views/petugas/peminjaman/index.blade.php
+++ resources/views/petugas/peminjaman/index.blade.php
@@ -2,71 +2,102 @@
 @section('content')
     <div class="bg-white p-4 rounded shadow-md">
         <div class="flex justify-between items-center">
-            <div class="font-bold text-lg w-full">
-                <span>Data {{ $title }}</span>
-            </div>
-            <div class="flex gap-2 text-white text-sm w-full">
-                <a href="{{ route('petugas.peminjaman.printPeminjaman') }}" target="_blank" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-print"></i>
-                    <span>Print</span>
+            <h2 class="font-semibold text-lg">Data {{ $title }}</h2>
+            <div class="flex gap-2 text-sm">
+                <a href="{{ route('petugas.peminjaman.printPeminjaman') }}" target="_blank" class="px-4 py-2 rounded bg-blue-900 text-white flex items-center gap-2 hover:bg-blue-950">
+                    <i class="fa-solid fa-print"></i> Print
                 </a>
-                <a href="" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-file-export"></i>
-                    <span>Eksport</span>
+                <a href="#" class="px-4 py-2 rounded bg-blue-900 text-white flex items-center gap-2 hover:bg-blue-950">
+                    <i class="fa-solid fa-file-export"></i> Export
                 </a>
-                <a href="{{ route('petugas.peminjaman.importPeminjaman') }}" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-file-import"></i>
-                    <span>Import</span>
+                <a href="{{ route('petugas.peminjaman.importPeminjaman') }}" class="px-4 py-2 rounded bg-blue-900 text-white flex items-center gap-2 hover:bg-blue-950">
+                    <i class="fa-solid fa-file-import"></i> Import
                 </a>
-                <a href="{{ route('petugas.peminjaman.addPeminjaman') }}" class="p-2 w-full justify-center rounded bg-blue-900 flex gap-1 items-center hover:bg-blue-950">
-                    <i class="fa-solid fa-plus"></i>
-                    <span>Tambah</span>
+                <a href="{{ route('petugas.peminjaman.addPeminjaman') }}" class="px-4 py-2 rounded bg-blue-900 text-white flex items-center gap-2 hover:bg-blue-950">
+                    <i class="fa-solid fa-plus"></i>Tambah
                 </a>
-                <a href="" class="py-2 px-4 justify-center rounded bg-red-500 flex gap-1 items-center hover:bg-red-600">
+                <a href="" class="px-4 py-2 rounded bg-red-500 text-white flex items-center gap-2 hover:bg-red-600">
                     <i class="fa-solid fa-trash-can-arrow-up"></i>
                 </a>
             </div>
         </div>
-        <div class="mt-4">
-            <table class="text-sm" id="peminjamanTable">
-                <thead class="w-full">
-                    <tr>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Peminjam</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Buku</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Petugas</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Tanggal Peminjaman</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Tanggal Pengembalian</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Tanggal Dikembalikan</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">Status</td>
-                        <td class="p-2 text-center font-bold uppercase bg-slate-200">option</td>
+    </div>
+    <div class="bg-white p-4 rounded shadow-md mt-4">
+        <div class="overflow-x-auto">
+            <table id="peminjamanTable" class="min-w-full border border-gray-400 text-sm">
+                <thead class="bg-gray-400 w-full">
+                    <tr class="text-gray-950">
+                        <th class="p-2 text-center font-bold uppercase relative">Peminjam</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Buku</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Petugas</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Tanggal Peminjaman</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Tanggal Pengembalian</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Tanggal Dikembalikan</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Status</th>
+                        <th class="p-2 text-center font-bold uppercase relative">Option</th>
                     </tr>
                 </thead>
                 <tbody class="w-full">
-                    {{-- @foreach ($dataKategori as $data)
-                        <tr>
-                            <td class="p-2">{{ $data->kategori }}</td>
-                            <td class="p-2 flex gap-2 text-center justify-center">
-                                <a href="" class="py-1 px-2 rounded text-center bg-blue-500 text-white">
-                                    <i class="fa-solid fa-pencil text-sm"></i>
-                                </a>
-                                <form id="deleteKategori" action="{{ route('petugas.kategori.deleteKategori', $data->id) }}" method="POST" class="hidden">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
-                                <button class="py-1 px-2 rounded text-center bg-red-500 text-white" onclick="deleteKategori()">
-                                    <i class="fa-solid fa-trash text-sm"></i>    
-                                </button>
+                    {{-- @foreach ($dataBuku as $item) --}}
+                        <tr class="border-b border-gray-400 hover:bg-gray-50">
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2 text-left">...</td>
+                            <td class="p-2">
+                                <div class="flex gap-2 justify-center items-center">
+                                    <a href="" class="py-1 px-2 rounded text-center bg-blue-900 hover:bg-blue-950 text-white">
+                                        <i class="fa-solid fa-pencil text-sm"></i>
+                                    </a>
+                                    <form id="deleteBuku" action="" method="POST" class="">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="py-1 px-2 rounded text-center bg-red-500 hover:bg-red-600 text-white" onclick="deleteBuku()">
+                                            <i class="fa-solid fa-trash text-sm"></i>    
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
-                    @endforeach --}}
+                    {{-- @endforeach --}}
                 </tbody>
             </table>
         </div>
     </div>
+
+    <style>
+        .dataTables_wrapper .dataTables_filter, 
+        .dataTables_wrapper .dataTables_length,
+        .dataTables_wrapper .dataTables_info,
+        .dataTables_wrapper .dataTables_paginate {
+            font-size: 0.25rem !important; 
+        }
+
+        th {
+            text-align: left !important;    
+        }
+    </style>
+
     <script>
-        $(document).ready( function () {
-            $('#peminjamanTable').DataTable();
-            
-        } );
+        $(document).ready(function() {
+            $('#peminjamanTable').DataTable({
+                responsive: true,
+                autoWidth: false,
+                language: {
+                    search: "Cari:",
+                    lengthMenu: "Show _MENU_",
+                    info: "Show _START_ to _END_ of _TOTAL_ entries",
+                    paginate: {
+                        first: "<<",
+                        last: ">>",
+                        next: ">",
+                        previous: "<"
+                    }
+                }
+            });
+        });
     </script>
 @endsection
